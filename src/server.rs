0142ba6586e937ea3d10bc6900a54989008c519d
Kelton Bassingthwaite <keltonbassingthwaite@gmail.com>
2022-12-25 17:08:24 -0800

    Server is configured using config struct.

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,23 @@
-pub async fn start() {
-    warp::serve(warp::fs::dir("."))
-        .run(([127, 0, 0, 1], 4750))
+use std::{
+    net::{IpAddr, SocketAddr},
+    str::FromStr,
+};
+
+use crate::config::Config;
+
+pub async fn handle_remote(cfg: Config) {}
+pub async fn handle_local(cfg: Config) {
+    return start(cfg).await;
+}
+
+async fn start(cfg: Config) {
+    let bind_address =
+        match SocketAddr::from_str(&format!("{}:{}", cfg.server.bind_address, cfg.server.port)) {
+            Ok(val) => val,
+            Err(_) => panic!("Could not parse bind_address!"),
+        };
+    println!("Serving {} on {}", cfg.directory, bind_address);
+    warp::serve(warp::fs::dir(cfg.directory))
+        .run(bind_address)
         .await;
 }
